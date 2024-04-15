ded5352af1b5181f00e124834d079575a96b2c70
Jacky <29943110+jw-12138@users.noreply.github.com>
2024-04-15 14:24:37 +0800

    update

---
+++ update.mjs
@@ -0,0 +1,27 @@
+import fs from 'fs'
+
+const fixedMarkdownContent = `## Hello there <sup>👋</sup>  
+
+My name is **_Jacky_**.  
+
+I'm a:  
+
+- Front-end Developer <sup>💻</sup>   
+- Music Producer <sup>🎵</sup>      
+
+Latest update on my blog:
+  
+`
+
+let newPostsMarkdownContent = ''
+
+let remoteContent = await fetch('https://jw1.dev/posts.json')
+
+let posts = await remoteContent.json()
+
+// keep only 5
+posts.slice(0, 5).forEach(post => {
+  newPostsMarkdownContent += `- [${post.title}](https://${post.url}) \n`
+})
+
+fs.writeFileSync('./README.md', fixedMarkdownContent + newPostsMarkdownContent)
